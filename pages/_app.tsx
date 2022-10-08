8c99d2aa657d8ca6f9d90e77f803763edc52c72d
thebuilderdesign <92240812+thebuilderdesign@users.noreply.github.com>
2022-10-09 04:12:21 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
+import Header from "../components/Header";
+
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Arbitrum;
@@ -8,7 +12,21 @@ const activeChainId = ChainId.Arbitrum;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>Goblintrum</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Goblintrum"
+        />
+        <meta
+          name="keywords"
+          content="Goblintrum"
+        />
+      </Head>
+      <Header />
       <Component {...pageProps} />
+      <ThirdwebGuideFooter />
     </ThirdwebProvider>
   );
 }
